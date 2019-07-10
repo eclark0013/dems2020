bf30911d84258559941fc2d8c00accd947779c9e
Eric Clark <eclark0013@gmail.com>
2019-07-10 14:15:18 +0000

    stubbed out CLI with fake data

--- lib/dems2020/cli.rb
+++ lib/dems2020/cli.rb
@@ -8,18 +8,11 @@ class Dems2020::CLI
     while input == "C" || input == "R" # creates a loop that allows user to continually access a list of candidates or race-info
       if input == "C" # display list of candidates and use @candidates which has already been scraped from ballotbedia
         display_candidates
-        input = gets.strip
-        until input.to_i.between?(1, @candidates.count)
-          puts "Choose from a number in the list above."
-          input = gets.strip
-        end
-        chosen_candidate = @candidates[input.to_i-1]
-        puts "wikipedia info of #{chosen_candidate}"
-        puts "Would you like to know about another candidate or about the race in general? Type C for candidate or R for race."
+        manage_candidate_choice
         input = gets.strip
       elsif input == "R"
         display_race_info
-        puts "Would you like to know about another candidate or about the race in general? Type C for candidate or R for race."
+        manage_race_info_choice
         input = gets.strip
       else
         puts "uh-oh, how did this happen?"
@@ -40,8 +33,45 @@ class Dems2020::CLI
     puts "Select a candidate from the list above using the number beside their name."
   end
 
-  def display_race_info
-    puts "dates and stuff"
+  def manage_candidate_choice
+    input = gets.strip
+    until input.to_i.between?(1, @candidates.count)
+      puts "Choose from a number in the list above."
+      input = gets.strip
+    end
+    chosen_candidate = @candidates[input.to_i-1]
+    puts "wikipedia info of #{chosen_candidate}"
+    puts "Would you like to know about another candidate or about the race in general? Type C for candidate or R for race."
   end
+
+  def display_race_info
+    puts "    What would you like to know?
+          \"C\" for a list of candidates still in the race
+          \"P\" for most recent polling data
+          \"D\" for important dates including upcoming debates, primaries and conventions"
+  end
+
+  def manage_race_info_choice
+    input = gets.strip
+    until input == "C" || input == "P" || input == "D"
+      puts "Choose from the options below:
+        \"C\" for a list of candidates still in the race
+        \"P\" for most recent polling data
+        \"D\" for important dates including upcoming debates, primaries and conventions"
+      input = gets.strip
+    end
+    if input == "C"
+      puts "Here is a list of candidates still in the race"
+    elsif input == "P"
+      puts "Here is the most recent polling data"
+    elsif input == "D"
+      puts "Here are some important dates."
+    else
+      puts "uh-oh, you weren't supposed to see this"
+    end
+    puts "Would you like to know about another candidate or about the race in general? Type C for candidate or R for race."
+  end
+
+
 
 end
