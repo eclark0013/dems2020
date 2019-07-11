f69d5c524a5f22acf3fafb1160562d0ba42fa082
Eric Clark <eclark0013@gmail.com>
2019-07-11 20:37:54 +0000

    candidate list

--- lib/dems2020/ballotpediascraper.rb
+++ lib/dems2020/ballotpediascraper.rb
@@ -3,11 +3,19 @@ require "./lib/environment"
 class BallotpediaScraper
 
   def self.candidates # https://ballotpedia.org/Presidential_candidates,_2020
-
-    candidates = ["Person 1", "Person 2", "Person 3"]
+    candidates = []
     doc = Nokogiri::HTML(open("https://ballotpedia.org/Presidential_candidates,_2020"))
-    doc.css("div.mobile-columns a").first.text
-    binding.pry
+    i = 0
+    until candidates.include? "Andrew Yang"
+      candidates << doc.css("div.mobile-columns a")[i].text
+      i+=1
+    end
+    candidates.each do |name|
+      if name[0].match(/[A-Z]/) == nil
+        candidates.delete(name)
+      end
+    end
+  #  binding.pry
   end
 
   def self.dates # https://ballotpedia.org/Important_dates_in_the_2020_presidential_race
--- lib/dems2020/wikipediascraper.rb
+++ lib/dems2020/wikipediascraper.rb
@@ -4,10 +4,17 @@ class WikipediaScraper
 
   def self.candidate_info(name)
     # use the name argument to create a URL to scrape
+    name = "Joe Biden"
+    name.gsub(" ", "_")
+    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{name}"))
     info =  {
       name: name,
       bio: "They have done things"
     }
   end
+
+  def name_space_to_underscore(name)
+
+
 
 end
