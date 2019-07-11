f2b4d28733c533af2ce9bb129bf334b5f3495cf5
Eric Clark <eclark0013@gmail.com>
2019-07-11 23:23:32 +0000

    working on education and bio

--- lib/dems2020/wikipediascraper.rb
+++ lib/dems2020/wikipediascraper.rb
@@ -5,11 +5,14 @@ class WikipediaScraper
   def self.candidate_info(name)
     # use the name argument to create a URL to scrape
     age = nil
-    if name.end_with? "Castro"
-      name = "Julian Castro"
-    end
+    education = nil
     undersore_name = name.gsub(" ", "_")
-    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{undersore_name}"))
+    if name.end_with? "Castro"
+      underscore_name = "Julian_Castro"
+    elsif "Steve Bullock"
+      underscore_name = "Steve_Bullock_(American_politician)"
+    end
+    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{underscore_name}"))
 
     # doc.css("tbody tr td")[23].text.split("age")[1,2]
     # doc.css("tbody tr td")[23].text.include? "age&nbsp;"
@@ -19,10 +22,18 @@ class WikipediaScraper
       if piece_of_info.text.include? "(age"
         age = piece_of_info.text.split("age")[1][1,2]
       end
+  #    if piece_of_info.text.include? "College" || piece_of_info.text.include? "University"
+      if 1 == 1
+        education = "the university"
+      end
     end
+    bio = "doing stuff"
+  #  binding.pry
     info =  {
       name: name,
-      age: age
+      age: age,
+      education: education,
+      bio: bio
     }
     info
   end
