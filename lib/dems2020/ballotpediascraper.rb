ad8bb9cb7ca0f33741ff43ecd705bcd6040ab52a
Eric Clark <eclark0013@gmail.com>
2019-07-13 12:34:48 +0000

    working on dates

--- lib/dems2020/ballotpediascraper.rb
+++ lib/dems2020/ballotpediascraper.rb
@@ -18,7 +18,12 @@ class BallotpediaScraper
   #  binding.pry
   end
 
-  def self.dates # https://ballotpedia.org/Important_dates_in_the_2020_presidential_race
+  def self.dates # https://ballotpedia.org/Important_dates_in_the_2020_presidential_race scrollable-table-container
+    doc = Nokogiri::HTML(open("https://ballotpedia.org/Presidential_candidates,_2020"))
+    binding.pry
+    # doc.css("div.mw-content-ltr table div.bp-dropdown-menu")
+    # doc.css("div.mw-content-ltr table").css("div.scrollable-table-container")
+    doc.css("div.mobile-columns a")[i].text
     upcoming_debates = ["First debate - July 1", "Second Debate - August 1"]
   end
 
